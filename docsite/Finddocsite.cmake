6c2516d26eedd4f46a24d4e736fb7a26fe496ad7
Gerwin Klein <gerwin.klein@proofcraft.systems>
2025-07-28 10:06:38 +1000

    trivial: apply new cmake-format 0.6.13 style
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- docsite/Finddocsite.cmake
+++ docsite/Finddocsite.cmake
@@ -4,38 +4,32 @@
 # SPDX-License-Identifier: BSD-2-Clause
 #
 
-set(DOCSITE_DIR "${CMAKE_CURRENT_LIST_DIR}" CACHE STRING "")
-set(DOCSITE_RUN_SCRIPT "${DOCSITE_DIR}/unpack_site.sh" CACHE STRING "")
+set(DOCSITE_DIR
+    "${CMAKE_CURRENT_LIST_DIR}"
+    CACHE STRING ""
+)
+set(DOCSITE_RUN_SCRIPT
+    "${DOCSITE_DIR}/unpack_site.sh"
+    CACHE STRING ""
+)
 mark_as_advanced(DOCSITE_DIR DOCSITE_RUN_SCRIPT)
 
 macro(docsite_build_site_tar outfile)
     include(ExternalProject)
     ExternalProject_Add(
         docsite
-        GIT_REPOSITORY
-        https://github.com/seL4/docs.git
-        GIT_TAG
-        gh-pages
-        GIT_SHALLOW
-        TRUE
-        GIT_PROGRESS
-        TRUE
-        CONFIGURE_COMMAND
-        "" # Don't run configure command
-        BUILD_ALWAYS
-        ON
-        BUILD_IN_SOURCE
-        TRUE
-        USES_TERMINAL_BUILD
-        TRUE
-        BUILD_COMMAND
-        "" # Nothing to build, gh-pages contains pre-built site
-        INSTALL_COMMAND
-        "rm;-rf;Hardware/CEI_TK1_SOM/"
-        COMMAND
-        "tar;-cvzf;../site.tar.gz;-C;..;docsite"
-        COMMAND
-        "mv;../site.tar.gz;${CMAKE_CURRENT_BINARY_DIR}"
+        GIT_REPOSITORY https://github.com/seL4/docs.git
+        GIT_TAG gh-pages
+        GIT_SHALLOW TRUE
+        GIT_PROGRESS TRUE
+        CONFIGURE_COMMAND "" # Don't run configure command
+        BUILD_ALWAYS ON
+        BUILD_IN_SOURCE TRUE
+        USES_TERMINAL_BUILD TRUE
+        BUILD_COMMAND "" # Nothing to build, gh-pages contains pre-built site
+        INSTALL_COMMAND "rm;-rf;Hardware/CEI_TK1_SOM/"
+        COMMAND "tar;-cvzf;../site.tar.gz;-C;..;docsite"
+        COMMAND "mv;../site.tar.gz;${CMAKE_CURRENT_BINARY_DIR}"
         EXCLUDE_FROM_ALL
     )
     include(external-project-helpers)
